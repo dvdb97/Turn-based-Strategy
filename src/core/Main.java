a69e221a27600f8bb89c8918958b64b3f723064e
dvdb97 <dario.vandenboom@arcor.de>
2018-01-08 21:49:06 +0100

    ¯\_(ツ)_/¯

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,15 +1,6 @@
 package core;
 
 import core.saves.StartParams;
-import graphics.matrices.Matrices;
-import interaction.PlayerCamera;
-import interaction.tileSelection.TileBuffer;
-import math.matrices.Matrix33f;
-import math.matrices.Matrix44f;
-import math.matrices.advanced.Determinant;
-import math.matrices.advanced.LU_Decomposition;
-import math.matrices.advanced.MatrixInversion33f;
-import math.matrices.advanced.MatrixInversion44f;
 
 public class Main {
 	
