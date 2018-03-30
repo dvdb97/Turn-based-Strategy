231455af6c6fc4b5f43dfc4624a69997a93a88db
dvdb97 <dario.vandenboom@arcor.de>
2018-03-30 13:03:40 +0200

    Removed: GUILayout

--- src/gui/test/ImplementedWindow.java
+++ src/gui/test/ImplementedWindow.java
@@ -1,6 +1,7 @@
 package gui.test;
 
 import elements.containers.GUIWindow;
+import elements.input.GUIToggleButton;
 import fontRendering.font.classic.TimesNewRoman;
 import math.vectors.Vector4f;
 import rendering.shapes.implemented.GUIQuad;
@@ -10,7 +11,9 @@ public class ImplementedWindow extends GUIWindow {
 	public ImplementedWindow(Vector4f color, float x, float y, float width, float height) {
 		super(new GUIQuad(), color, x, y, width, height);
 		
-		this.add(new TestToggleButton());
+		this.add(new GUIToggleButton(0.1f, -0.1f, 0.8f, 0.35f, "First!"));
+		
+		this.add(new GUIToggleButton(0.1f, -0.55f, 0.8f, 0.35f, "Second!"));
 		
 	}
 	
