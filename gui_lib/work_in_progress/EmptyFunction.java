cd0e8d6e071dacb09f312458aafad519d838a23f
Michael Brach <jona.brach@gx.de>
2018-05-21 11:49:57 +0200

    ADDED: EmptyFunction

---
+++ gui_lib/work_in_progress/EmptyFunction.java
@@ -0,0 +1,16 @@
+package work_in_progress;
+
+/**
+ * an implementation of the FunctionalInterface Function, that does nothing when executed
+ * 
+ * @author jona
+ *
+ */
+class EmptyFunction implements Function {
+	
+	@Override
+	public void execute(ElementBase element) {
+		//do nothing
+	}
+
+}
