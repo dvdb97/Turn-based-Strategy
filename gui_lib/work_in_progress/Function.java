4f47a1afd3375c6a2dcff2a058cb2cbd4084592c
C137-Jona <jona.brach@gmx.de>
2018-05-22 11:05:12 +0200

    EXPANDED: gui: Function

--- gui_lib/work_in_progress/Function.java
+++ gui_lib/work_in_progress/Function.java
@@ -1,9 +1,15 @@
 package work_in_progress;
 
+//this interface is similar to java.util.function.Consumer
+
 @FunctionalInterface
 public interface Function<E extends Element> {
 	
-	public abstract void execute(E element);
+	void execute(E element);
+	
+	default Function<E> andThen(Function<E> function) {
+		return (E element) -> {this.execute(element);function.execute(element);};
+	}
 	
 }
 
