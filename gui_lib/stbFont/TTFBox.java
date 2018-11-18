05384cb9a5060a57900f6074b56056823cf0bb5e
C137-Jona <jona.brach@gmx.de>
2018-11-18 16:37:44 +0100

    setup

---
+++ gui_lib/stbFont/TTFBox.java
@@ -0,0 +1,5 @@
+package stbFont;
+
+public class TTFBox {
+
+}
--- src/gui/GameGUIManager.java
+++ src/gui/GameGUIManager.java
@@ -1,15 +1,6 @@
 package gui;
 
-import dataType.GUIElementMatrix;
 import gui.tileInfo.TileInfoWindow;
-import interaction.TileSelecter;
-import rendering.shapes.GUIQuad;
-import work_in_progress.test.DebugWindow;
-import work_in_progress.test.DragableWindow;
-import work_in_progress.test.TestWindow;
-import world.gameBoard.GameBoard;
-
-import static utils.ColorPalette.*;
 
 public class GameGUIManager {
 	
@@ -17,21 +8,10 @@ public class GameGUIManager {
 	
 	public static void init() {
 		
-		//Disable this to remove the gui window from the screen
-	//	TIW = new TileInfoWindow();
-		
-		TestWindow window = new TestWindow();
-		
-		DebugWindow dw = new DebugWindow();
-		
-	//	DragableWindow window2 = new DragableWindow(new GUIQuad(), WHITE, new GUIElementMatrix(0.2f, 0.2f, 0.4f, 0.4f));
-		
 	}
 	
 	public static void update() {
-		
-	//	TIW.setTile(GameBoard.getTile(TileSelecter.getSelectedTileIndex()));
-		
+				
 	}
 	
 }
