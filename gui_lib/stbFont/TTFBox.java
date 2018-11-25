e1cf9fc9e60375d8e10082ceca983e87c890f2e1
C137-Jona <jona.brach@gmx.de>
2018-11-25 17:59:45 +0100

    refactoring

--- gui_lib/stbFont/TTFBox.java
+++ gui_lib/stbFont/TTFBox.java
@@ -21,24 +21,25 @@ import org.lwjgl.stb.STBTruetype;
 public class TTFBox extends Element {
 	
 	private Texture2D font;
+	private STBTTFontinfo fontInfo;
+	private int[] width = new int[1], height = new int[1];
+	private float scale;
 	
 	public TTFBox(float xShift, float yShift, float reqHeight) {
 		super(new GUIQuad(), null, new GUIElementMatrix(xShift, yShift, 1, reqHeight));
 		
-		ByteBuffer container = BufferUtils.createByteBuffer(160);
-		STBTTFontinfo fontInfo = new STBTTFontinfo(container);
-		int[] width = new int[1], height = new int[1];
+		setUpSTBFontinfo();
 		
 		try {
-			ByteBuffer data = getFontData();
-			STBTruetype.stbtt_InitFont(fontInfo, data, 0);
+			initFont();
 			
-			float pixelHeight = Utils.getHeightInPx(reqHeight);
-			float scale = STBTruetype.stbtt_ScaleForPixelHeight(fontInfo, pixelHeight);
+			scale = scaleForReqHeight(reqHeight);
+			
+		//	ByteBuffer[] bitmaps = new ByteBuffer
 			
 			ByteBuffer bitmap = STBTruetype.stbtt_GetCodepointBitmap(fontInfo, scale, scale, 'f', width, height, null, null);
-			ByteBuffer newBitmap = getNewBitmap(bitmap);
-			font = new Texture2D(newBitmap, width[0], height[0]);
+			ByteBuffer coloredBitmap = getColoredBitmap(bitmap);
+			font = new Texture2D(coloredBitmap, width[0], height[0]);
 			
 			elementMatrix.setXStretch(reqHeight*width[0]/height[0]*Application.getWindowHeight()/Application.getWindowWidth());
 			
@@ -47,8 +48,19 @@ public class TTFBox extends Element {
 		}
 		
 	}
+
+
+	private void setUpSTBFontinfo() {
+		ByteBuffer container = BufferUtils.createByteBuffer(160);
+		fontInfo = new STBTTFontinfo(container);
+	}
 	
-	private ByteBuffer getNewBitmap(ByteBuffer bitmap) {
+	private float scaleForReqHeight(float reqHeight) {
+		float pixelHeight = Utils.getHeightInPx(reqHeight);
+		return STBTruetype.stbtt_ScaleForPixelHeight(fontInfo, pixelHeight);
+	}
+
+	private ByteBuffer getColoredBitmap(ByteBuffer bitmap) {
 		ByteBuffer newBitmap = BufferUtils.createByteBuffer(bitmap.limit()*4);
 		for (int i=0; i<newBitmap.capacity();) {
 			newBitmap.put(bitmap.get());i++;
@@ -61,24 +73,24 @@ public class TTFBox extends Element {
 	}
 	
 	
-	private ByteBuffer getFontData() throws IOException{
+	private void initFont() throws IOException{
 		
 		DataInputStream data_in = new DataInputStream(
 				new BufferedInputStream(
 						new FileInputStream(
 								new File("res/fonts/ARIALBD.TTF"))));
-		ByteBuffer data_out = BufferUtils.createByteBuffer(1 << 20);
+		ByteBuffer fontData = BufferUtils.createByteBuffer(1 << 20);
 		
 		while(true) {
 			try {
 				byte b = data_in.readByte();
-				data_out.put(b);
+				fontData.put(b);
 				
 			} catch (EOFException eof) {break;}
 		}
 		data_in.close();
-		data_out.flip();
-		return data_out;
+		fontData.flip();
+		STBTruetype.stbtt_InitFont(fontInfo, fontData, 0);
 	}
 	
 	
